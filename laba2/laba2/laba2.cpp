b732386d24ec92d18334a9fa8f01271eb3529b65
Yasoaxi <vital1y-step@yandex.ru>
2025-09-11 17:10:03 +0300

    лаба 1

---
+++ laba2/laba2/laba2.cpp
@@ -0,0 +1,28 @@
+﻿
+
+#include <iostream>
+#include <string>
+using namespace ::std;
+
+int main()
+{
+    setlocale(LC_ALL, "Rus");
+    cout << "Задание 1\n";
+
+    double a, b;
+    cout << "Введите 2 числа: ";
+    cin >> a >> b;
+    double c = (a + b) / 2;
+    cout << "Среднее арифметическое: " << c << "\n";
+    cout << "Введите знак операции: + - *: ";
+    string answ;
+    cin >> answ;
+    if (answ == "+")
+        cout << a + b;
+    if (answ == "-")
+        cout << a - b;
+    if (answ == "*")
+        cout << a * b;
+
+}
+
