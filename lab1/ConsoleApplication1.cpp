dbf18c1cd307037f725f00d1e869c0e0fb62d291
sralityt-cmyk <sralityt@gmail.com>
2025-09-18 16:58:56 +0300

    lab1

---
+++ lab1/ConsoleApplication1.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "RU");
+    cout << "First string\n";
+    cout << "Первая строка\nВторая строка\n";
+    cout << "Спецсимволы \"\\\"";
+} 
