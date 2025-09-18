c2a5dfa7c977e648c5084260d62aa7530586e408
sralityt-cmyk <sralityt@gmail.com>
2025-09-18 17:29:27 +0300

    lab1

---
+++ lab1_2/lab1_2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 150;
+    float b = 15.933;
+    short  c = 250;
+    setlocale(LC_ALL, "RU");
+    cout << "First string\n";
+    cout << "Первая строка\nВторая строка\n";
+    cout << "Спецсимволы \"\\\"\n";
+    std::cout << "a = " << a << "\n";
+    std::cout << "b = " << b << "\n";
+    std::cout << "c = " << c << "\n";
+    short  d = 17;
+    short  m = 07;
+    short  y = 2006;
+    std::cout << d <<"." << m << "." << y << "\n";
+    float s = 2.3;
+    string o = "WINDOWS";
+    std::cout << s << " " << o << "\n";
+
+}
