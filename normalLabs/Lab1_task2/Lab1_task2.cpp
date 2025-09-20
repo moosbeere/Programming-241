8f02e64f6748d7f2ce111a9adc82972741efeadf
Znkrscht <krutatovfedous@gmail.com>
2025-09-20 15:36:33 +0300

    full Lab1

---
+++ normalLabs/Lab1_task2/Lab1_task2.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    int first = 150;
+    float second = 15.933;
+    short third = 250;
+    cout << "first = " << first << endl;
+    cout << "second = " << second << endl;
+    cout << "third = " << third << endl;
+
+    int day = 26;
+    int month = 7;
+    int year = 2006;
+    cout << "Моя дата рождения: " << day << " " << setw(2) << setfill('0') << month << " " << year << " года";
+
+    const float lastone = 2.3;
+    const string lasttwo = "WINDOWS";
+    cout << endl << lastone << " " << lasttwo;
+}
