d8b5563d7018000032964abb12ffce66c33069e1
moosbeere_o <moosbeere_O@mail.ru>
2025-09-25 17:15:32 +0300

    LAb2 function finish

--- 131/Lab2/Lab2.cpp
+++ 131/Lab2/Lab2.cpp
@@ -5,8 +5,20 @@
 #include "Windows.h"
 using namespace std;
 
+void sayHello();
+void sayHello(string name);
+void sayName(string name);
+int sum(int x, int y, bool pr = false);
+
+
 int main()
 {
+    //function
+    sum(4, 6, true);
+    sayHello();
+    sayHello("mira");
+    sayName("olga");
+
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
     //string name;
@@ -32,12 +44,12 @@ int main()
 
     bool b = true;
     if (b) {
-        cout << "ok" << endl;
+        //cout << "ok" << endl;
     }
     else if (!b) {
-        cout << "no" << endl;
+        //cout << "no" << endl;
     }
-    else cout << "!\n";
+    //else cout << "!\n";
 
     //int x = 7;
     //if (x < 10) x = 10;
@@ -62,7 +74,7 @@ int main()
     //cout << i; ошиибка обращения к локальной области видимости цикла
     int i = 1;
     while (i <= 5) {
-        cout << i << "\n";
+        //cout << i << "\n";
         i++;
         if (i == 3) continue;
         if (i == 4) break;
@@ -71,8 +83,73 @@ int main()
     //    cout << i << endl;
     //    i++;
     //} while (i <= 10);
+
+    int numbers[5];
+    numbers[0] = 45;
+    numbers[1] = 54;
+    numbers[2] = 76;
+    numbers[3] = 65;
+    numbers[4] = 78;
+    int len = sizeof(numbers) / sizeof(numbers[0]);
+    for (int i = 0; i < len; i++) {
+        //cout << i << ": " << numbers[i] << endl;
+    }
+    int numbers2[4] = { 2,5,8,3 };
+    int sum = 0;
+    for (auto e : numbers2) { // если заранее неизвестен тип данных элементов массива
+        sum += e;// sum = sum + e
+    }
+
+    int numbers3[] = { 2,5,8,3,7 };
+
+    for (int e : numbers3) {
+        sum += e;// sum = sum + e
+    }
+    //cout << sum << endl;
+
+    //многомерные массивы
+    //int array[3][2] = { {2,5}, {6,8}, {98, 7} };
+    //for (auto &e : array) {
+    //    for (int i : e) {
+    //        cout << i << ", ";
+    //    }
+    //    cout << endl;
+    //}
+    //cout << array[0][0] << endl;
+
+    //for (int i = 0; i < 3; i++) {
+    //    for (int j = 0; j < 2; j++) {
+    //        for (int j = 0; j < 2; j++) {
+    //        //cout << array[i][j] << ", ";
+    //    }
+    //    //cout << endl;
+    //}
+
+    //ссылки
+    int x = 14;
+    int& y = x;
+    cout << y << endl;
+    y = 15;
+    cout << x << endl;
 }
 
+void sayHello() {
+    cout << "Hello, world!\n";
+}
+
+void sayName(string name) {
+    cout << name << "\n";
+}
+
+void sayHello(string name) {
+    cout << "Hello, " << name << endl;
+}
+
+int sum(int x, int y, bool pr = false) {
+    int s = x + y;
+    if (pr) cout << s << endl;
+    return s;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
