a3b047cd0198691cea6fb00395cd9f7c20daf0c9
em1l4k <emilsumovasyj@gmail.com>
2025-09-25 09:56:40 +0300

    lab2: replace with empty skeleton

--- lab2.cpp
+++ lab2.cpp
@@ -1,19 +1,6 @@
-// Lab 2: Read two numbers and print their arithmetic mean
 #include <iostream>
 
 int main() {
-    double firstNumber = 0.0;
-    double secondNumber = 0.0;
-
-    std::cout << "Введите два числа: ";
-    if (!(std::cin >> firstNumber >> secondNumber)) {
-        std::cerr << "Ошибка ввода" << std::endl;
-        return 1;
-    }
-
-    const double arithmeticMean = (firstNumber + secondNumber) / 2.0;
-    std::cout << "Среднее арифметическое: " << arithmeticMean << std::endl;
-
-    return 0;
+   
 }
 
