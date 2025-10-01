4826b91faa7e02d5508055378679cb45df8f176f
corrupt3dk1d <d3ass13@ya.ru>
2025-10-01 23:18:04 +0300

    lab5 is complete

---
+++ lab5/task2/task2/MagicWeapon.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Weapon.h"
+
+
+class MagicWeapon :
+    public Weapon
+{
+    float extra_damage;
+public:
+    MagicWeapon(std::string name, float damage, float weight, float extra_damage, Specific type);
+    MagicWeapon(std::string name, float damage, float weight, float extra_damage);
+    MagicWeapon();
+    float getExtraDmg();
+    void setExtraDmg(float dmg);
+    void printAll() override;
+    float getDamage() override;
+    //bool operator> (const MagicWeapon& w2);
+};
+
