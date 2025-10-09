1b5179b67740e844e117a01e46cc329442545e1f
niluning <nilufartt50@gmail.com>
2025-10-09 16:39:01 +0300

    Add files via upload
    1 task toshboeva

---
+++ n.1 task toshoeva.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    //3 
+    setlocale(LC_ALL, "Russian");
+
+    //  1
+    cout << "First string" << endl;
+
+    //  2
+    cout << "Первая строка\nВторая строка" << endl;
+
+    // 4
+    cout << "Спецсимволы \"\\\"." << endl;
+
+    return 0;
+}
