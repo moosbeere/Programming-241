e7d59e5ff47b050b974bf1887d5e38e5f23f9f90
bulakhoverik <nnnnnn060606@yandex.ru>
2025-10-09 16:41:48 +0300

    Add files via upload

---
+++ laba1task2/laba1task2/laba1task2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int a = 150;
+    float b = 15.933;
+    short c = 250;
+    cout << "a = " << a << " b = " << b << " c = " << c << endl;
+
+    int day = 23;
+    string month = "july";
+    int year = 2006;
+    cout << "Моя дата рожения : " << day << "\t" << month << "\t" << year << "\n";
+
+    float const d = 2.3;
+    string const win = "WINDOWS";
+
+    cout << d << " " << win << endl;
+}
