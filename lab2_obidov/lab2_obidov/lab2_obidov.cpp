8392246cb7092b217387490cb11a8ce87b980e2b
erajjan <rauf2017.18@gmail.com>
2025-10-14 12:47:14 +0300

    2 laba

---
+++ lab2_obidov/lab2_obidov/lab2_obidov.cpp
@@ -0,0 +1,82 @@
+﻿	#include <iostream>
+using namespace std;
+//
+//int main()
+//{
+//    cout << "введите первое число:";
+//    int a;
+//    int b;
+//    cin >> a; cout << "Введите второе число:";  cin >> b;
+//    cout << "операция:";
+//    char c;
+//    cin >> c;
+//
+//    if (c == '*') {
+//        cout << a * b;
+//    }
+//    
+//    else if (c == '+') {
+//        cout << a + b;
+//    }
+//    else if (c == '-') {
+//        cout << a - b;
+//    }
+//    else if (c == '/') {
+//        if (b != 0) {
+//            cout << a / b;
+//        }
+//        else {
+//            cout << "Ошибка: деление на ноль";
+//        }
+//    }
+//    else {
+//        cout << "Неизвестная операция";
+//    }
+//    switch (c)
+//    {
+//    case'*':
+//         cout<<a * b;  
+//        break;
+//    case'/':
+//        cout << a / b;
+//        break;
+//    case'-':
+//        cout << a - b;
+//        break;
+//    case'+':
+//        cout << a + b;
+//        break;
+//    default:
+//        cout << "Введите только один из следующих символов:+,-,*,/";
+//        break;
+//    }
+//
+//}
+
+//2
+
+int main() {
+	cout << "vvedite chislo";
+	int a;
+	cin >> a;
+	int sum = 0;
+	while (true) {
+		if (a <= 0) {
+			cout << "vvedite chislo";
+			cin >> a;
+		}
+		else {
+			break;
+		}
+		}
+	
+	for (int i = 1; i <= a; i++) {
+		sum += i;
+		cout << i;
+		if (i < a) {
+			cout << "+";
+			
+		}
+	}
+	cout <<"="<< sum;
+}
