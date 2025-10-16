d3807af5b47f3c181756e99ecd3ebe8b866d5dcd
Znkrscht <krutatovfedous@gmail.com>
2025-10-16 16:24:40 +0300

    3 грокаем алгоритмы

---
+++ grokaemAlgoritmy/Lab3_1/Lab3_1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <random>
+
+using namespace std;
+
+void quickSort(int arr[], int left, int right) {
+    if (left >= right) return;
+    int mid = arr[(left + right) / 2];
+    int i = left, j = right;
+    while (i <= j) {
+        while (arr[i] < mid) i++;
+        while (arr[j] > mid) j--;
+        if (i <= j) {
+            swap(arr[i], arr[j]);
+            i++;
+            j--;
+        }
+    }
+    quickSort(arr, left, j);
+    quickSort(arr, i, right);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    const int s = 1000;
+    int array[s];
+    cout << "Первые 10 элементов исходного массива:" << endl;
+    for (int i = 0; i < s; i++) {
+        array[i] = rand() % 10000;
+        if (i < 10) cout << array[i] << " " ;
+    }
+    cout << endl;
+
+    quickSort(array, 0, 999);
+    cout << "Первые 10 элементов отсортированного списка:" << endl;
+    for (int i = 0; i < 10; i++) {
+        cout << array[i] << " ";
+    }
+}   
---
+++ grokaemAlgoritmy/Lab3_3/Lab3_3.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <random>
+#include <vector>
+
+using namespace std;
+
+void printMatrix(vector<vector<int>> m, const int r, const int c) {
+    for (int i = 0; i < r; i++) {
+        for (int j = 0; j < c; j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void quickSort(vector<vector<int>>& arr, int left, int right) {
+    if (left >= right) return;
+    int mid = arr[(left + right) / 2][0];
+    int i = left, j = right;
+    while (i <= j) {
+        while (arr[i][0] < mid) i++;
+        while (arr[j][0] > mid) j--;
+        if (i <= j) {
+            swap(arr[i][0], arr[j][0]);
+            i++;
+            j--;
+        }
+    }
+    quickSort(arr, left, j);
+    quickSort(arr, i, right);
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Ru");
+    const int ROWS = 10, COLS = 10;
+    vector<vector<int>> matrix(ROWS, vector<int>(COLS));
+    for (int i = 0; i < ROWS; i++) {
+        for (int j = 0; j < COLS; j++) {
+            matrix[i][j] = rand() % (61 - 6) + 6;
+        }
+    }
+    cout << "Первоначальная матрица:" << endl;
+    printMatrix(matrix, ROWS, COLS);
+    cout << endl;
+
+    quickSort(matrix, 0, 9);
+    cout << "Матрица после сортировки первого столбца:" << endl;
+    printMatrix(matrix, ROWS, COLS);
+}
