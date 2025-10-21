7a5a89fb21ee9d25ed85fa2220677e6f6e4d8297
Родион <rodkrylov@gmail.com>
2025-10-21 10:35:15 +0300

    1 задача 3 лаба

---
+++ Лабораторная работа №3. Быстрая сортировка/1 задача.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+void swap(int data[], int first, int second) {
+    auto temp = data[first];
+    data[first] = data[second];
+    data[second] = temp;
+}
+
+void sort(int data[], int start, int end) {
+    if (start >= end) return;
+    int current = start;
+    for (int i = start + 1; i <= end; i++) {
+        if (data[i] < data[start]) swap(data, ++current, i);
+    }
+    swap(data, current, start);
+    if (current > start) {
+        sort(data, start, current - 1);
+    }
+    if (end > current) {
+        sort(data, current + 1, end);
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int data[] = { 5, 3, 8, 2, 9, 1 };
+    sort(data, 0, size(data) - 1);
+    for (auto elem : data) {
+        cout << elem << ", ";
+    }
+}
+// не понимаю, в чем разница между 1 и 2 задачей, кроме как количеством элементов в массиве.
