3caee6549469d91d3a1c167aad225d7ab1614559
turkovr0623 <turkovroman2006@mail.ru>
2025-10-23 16:50:52 +0300

    Add files via upload

---
+++ ГА Лаб2.4/ГА Лаб2.4.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+void f(int n, int x = 2) {
+    if (n == 1) return;
+
+    if (n % x == 0) {
+        cout << x << " ";
+        f(n / x, x);
+    }
+    else {
+        f(n, x + 1);
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "RU");
+    cout << "Введите число: ";
+    int n;
+    cin >> n;
+    f(n);
+    return 0;
+}
