f28bec8f5e63b5e1f630d40c99a438126693ae88
turkovr0623 <turkovroman2006@mail.ru>
2025-10-30 16:11:39 +0300

    Add files via upload

---
+++ ГА Лаб3.2/ГА Лаб3.2.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+void qsort(int arr[], int left, int right)
+{
+    int i = left;
+    int j = right;
+    int pivot = arr[(left + right) / 2];
+
+    while (i <= j)
+    {
+        while (arr[i] < pivot)
+            i++;
+        while (arr[j] > pivot)
+            j--;
+        if (i <= j)
+        {
+            int x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j)
+        qsort(arr, left, j);
+    if (i < right)
+        qsort(arr, i, right);
+}
+
+int main()
+{
+    int a[20];
+    srand(time(0));
+    setlocale(LC_ALL, "RU");
+
+    for (int i = 0; i < 20; i++)
+    {
+        a[i] = rand() % 50 + 50;
+    }
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < 20; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    qsort(a, 0, 19);
+
+    cout << "\nОтсортированный массив: ";
+    for (int i = 0; i < 20; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
