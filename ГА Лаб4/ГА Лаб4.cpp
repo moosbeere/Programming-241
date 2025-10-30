3976d6f583f2c829b649596ebad2bfd5bee60734
turkovr0623 <turkovroman2006@mail.ru>
2025-10-30 16:14:11 +0300

    Add files via upload

---
+++ ГА Лаб4/ГА Лаб4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <unordered_set>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    unordered_set<int> s;
+    long long x;
+
+    while (cin >> x) {
+        if (x > 0) {
+            s.insert((int)x);
+        }
+        else if (x < 0) {
+            s.erase((int)(-x));
+        }
+        else {
+            vector<int> result(s.begin(), s.end());
+            sort(result.begin(), result.end());
+            for (int num : result) {
+                cout << num << " ";
+            }
+            cout << "\n";
+            return 0;
+        }
+    }
+
+    return 0;
+}
