32cba9e81859d79d176401f2542ffe6c74ec536d
turkovr0623 <turkovroman2006@mail.ru>
2025-10-30 16:10:49 +0300

    Add files via upload

---
+++ ГА Лаб3.1/ГА Лаб3.1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+using namespace std;
+
+void qsort(int arr[], int left, int right)
+{
+    int i = left;
+    int j = right;
+    int pivot = arr[(left + right) / 2];
+
+    while (i <= j)
+    {
+        while (arr[i] < pivot)
+            i++;
+        while (arr[j] > pivot)
+            j--;
+        if (i <= j)
+        {
+            int x = arr[i];
+            arr[i] = arr[j];
+            arr[j] = x;
+            i++;
+            j--;
+        }
+    }
+
+    if (left < j)
+        qsort(arr, left, j);
+    if (i < right)
+        qsort(arr, i, right);
+}
+
+int main()
+{
+    int a[1000];
+
+    for (int i = 0; i < 1000; i++)
+    {
+        a[i] = rand() % 10000;
+    }
+
+    qsort(a, 0, 999);
+
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
