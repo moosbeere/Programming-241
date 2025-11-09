5c1cd25195c78ad1f87019c3a48386eafa8214d6
corrupt3dk1d <d3ass13@ya.ru>
2025-11-09 17:35:28 +0300

    lab6 und lab7

---
+++ lab6/task1/task1/task1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <string>
+#include "MyException.cpp";
+
+int main()
+{
+
+	try {
+
+		std::cout << "Please input a number" << std::endl;
+		int n;
+		std::cin >> n;
+		if (n == 1) std::cout << "One" << std::endl;
+		else if (n == 2) std::cout << "Two" << std::endl;
+		else if (n == 3) std::cout << "Tree" << std::endl;
+		else throw "Exception: Incorrect number!";
+		std::cout << "Input your birth year";
+		int date;
+		std::cin >> date;
+		if (date < 1850) throw std::exception("It is not true");
+		else if (date > 2025) throw std::logic_error("You are not born yet.");
+		else if (date < 0) throw std::logic_error("You enterered a negative number");
+		std::cout << "Thank you, everything's good.";
+
+		/* Using MyException class */
+		std::cout << "Input your birth year again" << std::endl;
+		std::cin >> date;
+		if (date < 0) throw MyException(DateError::NEGATIVE);
+		else if (date < 1850) throw MyException(DateError::EARLY);
+		else if (date > 2025) throw MyException(DateError::LATE);
+	}
+	catch (const char* e)
+	{
+		std::cout << e << std::endl;
+	}
+	catch (std::logic_error e)
+	{
+		std::cout << "Logic Error: " << e.what() << std::endl;
+	}
+	catch (std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	
+
+}
---
+++ lab7/task2/task2/task2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <string>
+#include <regex>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+bool validName(std::string s)
+{
+	std::regex reg("[A-Z][a-z]{1,31}");
+	return (std::regex_match(s, reg) && std::isupper(s[0]));
+}
+
+//std::vector<std::string> findLinks(std::string s)
+//{
+//
+//}
+
+int main()
+{
+	std::string name;
+	std::cout << "Input your name:" << std::endl;
+	std::cin >> name;
+	if (validName(name))
+		std::cout << "The name is correct." << std::endl;
+	else
+		std::cout << "name is incorrect" << std::endl;
+
+	std::ifstream in("input.txt");
+	std::string text;
+	std::getline(in, text);
+	std::smatch matches;
+	//std::cout << "Input text with emails" << std::endl;  # Uncomment when text is not got from input.txt
+	//std::cin >> text;
+	std::regex reg(R"(\b[a-z0-9]{2,}@[a-z]{2,10}\.[a-z]{2,5}\b)", std::regex_constants::icase);
+
+	while (std::regex_search(text, matches, reg))
+	{
+		std::cout << matches[0] << ' ' << matches[1] << std::endl;
+		text = matches.suffix().str();
+		//std::cout << text << std::endl;
+	}
+
+
+}
