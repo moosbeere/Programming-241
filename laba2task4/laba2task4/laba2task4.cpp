14db3e52880a3234f33d5f660c957c7d422c9f1f
bulakhoverik <nnnnnn060606@yandex.ru>
2025-11-13 12:07:43 +0300

    Add files via upload

---
+++ laba2task4/laba2task4/laba2task4.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include "static_function.h"
+#include "static_function.cpp"
+
+using namespace std;
+using namespace StaticUtils;
+
+int main() {
+    cout << "Демонстрация работы со статической переменной:" << endl;
+
+    cout << "\nПервый вызов с числом 5:" << endl;
+    staticFunction(5);
+
+    cout << "\nВторой вызов с числом 10:" << endl;
+    staticFunction(10);
+
+    cout << "\nТретий вызов с числом 3:" << endl;
+    staticFunction(3);
+
+    return 0;
+}
