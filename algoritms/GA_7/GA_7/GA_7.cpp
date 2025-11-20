e57a368b713b83c70541bd1bcc2a3c045e2b05fc
erajjan <rauf2017.18@gmail.com>
2025-11-20 15:10:57 +0300

    1_chast

--- algoritms/GA_7/GA_7/GA_7.cpp
+++ algoritms/GA_7/GA_7/GA_7.cpp
@@ -4,71 +4,71 @@
 
 using namespace std;
 
-//void printPair(vector<pair<int, int>>& vec) {
-//    for (auto elem : vec) {
-//        cout << elem.first << " " << elem.second << endl;
-//    }
-//}
-//
-//bool compTime(pair<int, int>& a, pair<int, int>& b) {
-//    return a.second < b.second;
-//}
-//
-//int main()
-//{
-//    setlocale(LC_ALL, "Ru");
-//    int N;
-//    cout << "Введите количество заявок: ";
-//    cin >> N;
-//    vector<pair<int, int>> lectures(N);
-//    for (int i = 0; i < N; i++) {
-//        cin >> lectures[i].first >> lectures[i].second;
-//    }
-//    sort(lectures.begin(), lectures.end(), compTime);
-//    // printPair(lectures);
-//    int count = 0;
-//    int curr_end = 0;
-//    for (auto elem : lectures) {
-//        if (elem.first >= curr_end) {
-//            count++;
-//            curr_end = elem.second;
-//        }
-//    }
-//    cout << "Максимальное количество заявок, которые можно выполнить: " << count;
-//}
-
-
-
-//boxes
-void printArr(vector<pair<int, int>>& vec) {
+void printPair(vector<pair<int, int>>& vec) {
     for (auto elem : vec) {
         cout << elem.first << " " << elem.second << endl;
     }
 }
 
-bool compWeight(pair<int, int>& a, pair<int, int>& b) {
-    return a.first + a.second < b.first + b.second;
+bool compTime(pair<int, int>& a, pair<int, int>& b) {
+    return a.second < b.second;
 }
 
 int main()
 {
     setlocale(LC_ALL, "Ru");
-    int n;
-    cout << "Введите количество коробок: ";
-    cin >> n;
-    vector<pair<int, int>> presents(n);
-    for (int i = 0; i < n; i++) {
-        cin >> presents[i].first >> presents[i].second;
+    int N;
+    cout << "Введите количество заявок: ";
+    cin >> N;
+    vector<pair<int, int>> lectures(N);
+    for (int i = 0; i < N; i++) {
+        cin >> lectures[i].first >> lectures[i].second;
     }
-    sort(presents.begin(), presents.end(), compWeight);
-    // printArr(presents);
-    int total = 0;
+    sort(lectures.begin(), lectures.end(), compTime);
+     printPair(lectures);
     int count = 0;
-    for (auto elem : presents) {
-        if (total <= elem.second) {
+    int curr_end = 0;
+    for (auto elem : lectures) {
+        if (elem.first >= curr_end) {
             count++;
-            total += elem.first;
+            curr_end = elem.second;
         }
     }
-    cout << "Количество сложенных друг на друга коробок: " << count;
+    cout << "Максимальное количество заявок, которые можно выполнить: " << count;
 }
+
+
+
+//boxes
+//void printArr(vector<pair<int, int>>& vec) {
+//    for (auto elem : vec) {
+//        cout << elem.first << " " << elem.second << endl;
+//    }
+//}
+//
+//bool compWeight(pair<int, int>& a, pair<int, int>& b) {
+//    return a.first + a.second < b.first + b.second;
+//}
+//
+//int main()
+//{
+//    /*setlocale(LC_ALL, "Ru");*/
+//    int n;
+//    cout << "Введите количество коробок: ";
+//    cin >> n;
+//    vector<pair<int, int>> presents(n);
+//    for (int i = 0; i < n; i++) {
+//        cin >> presents[i].first >> presents[i].second;
+//    }
+//    sort(presents.begin(), presents.end(), compWeight);
+//     printArr(presents);
+//    int total = 0;
+//    int count = 0;
+//    for (auto elem : presents) {
+//        if (total <= elem.second) {
+//            count++;
+//            total += elem.first;
+//        }
+//    }
+//    cout << "Количество сложенных друг на друга коробок: " << count;
+//}
--- obvious lab/lab4/lab4/lab4.cpp
+++ obvious lab/lab4/lab4/lab4.cpp
@@ -3,38 +3,50 @@ using namespace std;
 #include <string>
 
 //1
-//class orujie {
-//public:
-//	float damage;
-//	string name;
-//	float weight;
-//	//2
-//	orujie(float d, string n, float w) {
-//		damage = d;
-//		name = n;
-//		weight = w;
-//
-//	}
-//	//3
-//	orujie() : orujie(10, "wrist", 1) {};
-//};
-//
-//
-////4
-//orujie crook = orujie(40, "crook", 2);
-//
-//int main() {
-//	//4
-//	orujie wrist = orujie();
-//	cout<<"Weight of the weapon = " << crook.weight << "\n";
-//	cout <<"name of the weapon:" << crook.name << "\n";
-//	cout <<"damage from 1 hit:" << crook.damage << "\n";
-//
-//	cout << "2 orujie";
-//	cout << "Weight of the weapon = " << wrist.weight << "\n";
-//	cout << "name of the weapon:" << wrist.name << "\n";
-//	cout << "damage from 1 hit:" << wrist.damage << "\n";
-//}
+class orujie {
+public:
+	float damage;
+	string name;
+	float weight;
+	//2
+	orujie(float d, string n, float w) {
+		damage = d;
+		name = n;
+		weight = w;
+
+	}
+	//3
+	orujie() : orujie(10, "wrist", 1) {};
+
+	//  2 destructor
+	~orujie() {
+		cout << damage<< name<< weight ;
+		cout << " уничтожаeтся;      ";
+	}
+};
+
+
+//4
+orujie crook = orujie(40, "crook", 2);
+
+int main() {
+	//4
+	orujie wrist = orujie();
+	cout<<"Weight of the weapon = " << crook.weight << "\n";
+	cout <<"name of the weapon:" << crook.name << "\n";
+	cout <<"damage from 1 hit:" << crook.damage << "\n";
+
+	cout << "2 orujie";
+	cout << "Weight of the weapon = " << wrist.weight << "\n";
+	cout << "name of the weapon:" << wrist.name << "\n";
+	cout << "damage from 1 hit:" << wrist.damage << "\n";
+}
+
+
+
+
+
+ 
 
 
 
