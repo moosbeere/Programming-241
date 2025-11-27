e4504f34972c0105799a1de3d2379018aa68d898
avenesu <yoandymail@gmail.com>
2025-11-27 17:21:58 +0300

    лаба номер three

---
+++ C++/Лаб_3_Задание_1/Лаб_3_Задание_1/Лаб_3_Задание_1.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+
+using namespace std;
+
+char s() {
+	return ' ';
+}
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+
+	int a = 30;
+	int b[] = { 1, 2, 3 };
+	int c = 5;
+	int* pointer = &a;
+	int* const pc = &c;
+
+	cout << "Значение: |   Адрес:" << endl;
+	cout << s() << *pointer << "       |    " << pointer << endl;
+	*pointer = 23;
+	cout << s() << *pointer << "       |    " << pointer << endl;
+
+	for (int i = 0; i < sizeof(b) / sizeof(b[0]); i++) {
+		cout << *(b + i) << s();
+	}
+	
+	cout << "Значение: |   Адрес:" << endl;
+	cout << s() << *pc << "       |    " << pc << endl;
+	//pc = &a; expression must be a modifiable lvalue E0137
+	*pc = 7;
+	cout << s() << *pc << "       |    " << pc << endl;
+
+}
---
+++ C++/Лаб_3_Задание_2/Лаб_3_Задание_2/Лаб_3_Задание_2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+
+using namespace std;
+
+void sp() {
+	cout << " ";
+}
+
+int sum(int first, int second) {
+	return first + second;
+}
+int dif(int first, int second) {
+	return first - second;
+}
+
+int (*charA(char a))(int, int) {
+	if (a == '+') {
+		return sum;
+	}
+	else if (a == '-') {
+		return dif;
+	}
+}
+
+int arrSum(int* first, int* last) {
+	int sumA = 0;
+	for (int* i = first; i < last; i++) {
+		sumA = sum(sumA, *i);
+	}
+	return sumA;
+}
+
+
+int main()
+{
+	int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
+	cout << arrSum(begin(arr), end(arr));
+	sp();
+	cout << sum(arr[3], arr[4]);
+	sp();
+	cout << dif(arr[3], arr[4]) << endl;
+	cout << charA('+');
+	sp();
+	int (*operation)(int, int) = charA('+');
+	cout << operation(arr[3], arr[4]);
+	float* dynamic = new float;
+	*dynamic = 3.14;
+	cout << *dynamic;
+	delete dynamic;
+}
+
