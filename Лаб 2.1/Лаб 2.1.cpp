d57c40596a8639106f022e73e0f80ba62e8a3e9f
turkovr0623 <turkovroman2006@mail.ru>
2025-11-27 16:54:45 +0300

    Add files via upload

---
+++ Лаб 2.1/Лаб 2.1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    double a, b;
+    cout << "Введите два числа: ";
+    cin >> a >> b;
+
+    cout << "Среднее арифметическое: " << (a + b) / 2 << endl;
+
+    char x;
+    cout << "Введите операцию (+ - * /): ";
+    cin >> x;
+
+    /*
+    if (x == '+') cout << a + b;
+    else if (x == '-') cout << a - b;
+    else if (x == '*') cout << a * b;
+    else if (x == '/') cout << a / b;
+    else cout << "Неизвестная операция!";
+    */
+
+    switch (x) {
+    case '+': cout << a + b; break;
+    case '-': cout << a - b; break;
+    case '*': cout << a * b; break;
+    case '/': cout << a / b; break;
+    default: cout << "Неизвестная операция!";
+    }
+
+    return 0;
+}
