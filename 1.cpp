3b3894889feeebecf009347110746cef847df467
Мэн Сяньлинь <mwe332@gmail.com>
2025-12-25 02:48:39 +0300

    Add files via upload

--- 1.cpp
+++ 1.cpp
@@ -4,12 +4,6 @@
 
 using namespace std;
 
-void fill(int arr[], int size, int minVal, int maxVal) {
-  for (int i = 0; i < size; i++) {
-    arr[i] = rand() % (maxVal - minVal + 1) + minVal;
-  }
-}
-
 void printArr(int arr[], int size, const string &label) {
   cout << label << ": ";
   for (int i = 0; i < size; i++) {
@@ -18,12 +12,17 @@ void printArr(int arr[], int size, const string &label) {
   cout << endl;
 }
 
-void printSArr(const string arr[], int size, const string &label) {
-  cout << label << ":\n";
+void printStringArr(const string arr[], int size, const string &label) {
+  cout << label << ":" << endl;
   for (int i = 0; i < size; i++) {
     cout << arr[i] << endl;
   }
   cout << endl;
+}
+void fillArr(int arr[], int size, int minVal, int maxVal) {
+  for (int i = 0; i < size; i++) {
+    arr[i] = rand() % (maxVal - minVal + 1) + minVal;
+  }
 }
 
 void selectionSort(int arr[], int size) {
@@ -40,7 +39,7 @@ void selectionSort(int arr[], int size) {
   }
 }
 
-void selectionSortRevers(int arr[], int size) {
+void selectionSortReversed(int arr[], int size) {
   for (int i = 0; i < size - 1; i++) {
     int maxIndex = i;
     for (int j = i + 1; j < size; j++) {
@@ -69,33 +68,36 @@ void sortPhoneNumbers(string arr[], int size) {
 }
 
 void task1() {
-  const int SIZE = 10;
-  int arr[SIZE];
+  const int size = 10;
+  int arr[size];
 
-  fill(arr, SIZE, 2, 103);
-  printArr(arr, SIZE, "До");
-  selectionSort(arr, SIZE);
-  printArr(arr, SIZE, "После");
+  fillArr(arr, size, 2, 103);
+
+  printArr(arr, size, "До сортировки");
+
+  selectionSort(arr, size);
+
+  printArr(arr, size, "После сортировки");
 }
 
 void task2() {
-  const int SIZE = 10;
-  int arr[SIZE];
+  const int size = 10;
+  int arr[size];
 
-  fill(arr, SIZE, 0, 100);
-  printArr(arr, SIZE, "До");
-  selectionSortRevers(arr, SIZE);
-  printArr(arr, SIZE, "После");
+  fillArr(arr, size, 0, 100);
+  printArr(arr, size, "До сортировки");
+  selectionSortReversed(arr, size);
+  printArr(arr, size, "После сортировки");
 }
 
 void task3() {
-  const int SIZE = 8;
-  string phoneNumbers[SIZE] = {"23-45-67", "12-34-56", "98-76-54", "11-22-33",
+  const int size = 8;
+  string phoneNumbers[size] = {"23-45-67", "12-34-56", "98-76-54", "11-22-33",
                                "55-44-33", "67-89-01", "45-67-89", "32-10-98"};
 
-  printSArr(phoneNumbers, SIZE, "До");
-  sortPhoneNumbers(phoneNumbers, SIZE);
-  printSArr(phoneNumbers, SIZE, "После");
+  printStringArr(phoneNumbers, size, "До");
+  sortPhoneNumbers(phoneNumbers, size);
+  printStringArr(phoneNumbers, size, "После");
 }
 
 int main() {
