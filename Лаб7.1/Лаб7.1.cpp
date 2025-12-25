e70f9367a0662a477f8db6c66b6720b5ae935d40
turkovr0623 <turkovroman2006@mail.ru>
2025-12-25 16:14:02 +0300

    Add files via upload

---
+++ Лаб7.1/Лаб7.1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    string s;
+    cout << "Введите строку: ";
+    getline(cin, s);
+    if (s.size() >= 4)
+    {
+        cout << "Подстрока (со 2 по 4 символ): "
+            << s.substr(1, 3) << endl;
+    }
+    else
+    {
+        cout << "Строка слишком короткая" << endl;
+    }
+    int index = s.find("a");
+    if (index != string::npos)
+    {
+        cout << "Индекс первого вхождения 'a': "
+            << index << endl;
+    }
+    else
+    {
+        cout << "Символа a не найдено" << endl;
+    }
+    return 0;
+}
