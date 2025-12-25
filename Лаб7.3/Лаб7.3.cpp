b6b37d66ffd7b164fe28a8414b6d156fbee7b5a2
turkovr0623 <turkovroman2006@mail.ru>
2025-12-25 16:16:14 +0300

    Add files via upload

---
+++ Лаб7.3/Лаб7.3.cpp
@@ -0,0 +1,64 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    double r;
+    cout << "Введите радиус окружности: ";
+    cin >> r;
+    const double PI = 3.141592653589793;
+    double length = 2 * PI * r;
+
+    double rounded = round(length * 1000) / 1000;
+
+    cout << "Длина окружности: " << rounded << endl;
+
+    cin.ignore();
+
+    string input;
+    fstream file;
+
+    while (true)
+    {
+        cout << "\nВведите строку: ";
+        getline(cin, input);
+
+        if (input == "exit")
+        {
+            break;
+        }
+
+        if (input == "read")
+        {
+            file.open("text.txt", ios::in);
+            string line;
+
+            while (getline(file, line))
+            {
+                cout << line << endl;
+            }
+
+            file.close();
+            continue;
+        }
+
+        if (input == "erase")
+        {
+            file.open("text.txt", ios::out | ios::trunc);
+            file.close();
+            cout << "Файл очищен\n";
+            continue;
+        }
+
+        file.open("text.txt", ios::out | ios::app);
+        file << input << endl;
+        file.close();
+    }
+
+    return 0;
+}
