faca49623ab66259a82fbf74d64b7eb2a6640474
Ulyana <100966619+ulyanka1233@users.noreply.github.com>
2025-12-25 17:18:41 +0300

    Update and rename MegicOrugie5.h to MegicOrugie.h

--- MegicOrugie.h
+++ MegicOrugie.h
@@ -2,18 +2,16 @@
 #include "Orugie.h"
 #include <iostream>
 
-// 7. Дочерний класс для магического оружия
 class MagicOrugie : public Orugie {
 private:
-    int DopUron;  // 8. дополнительный урон
+    int DopUron;  
 
 public:
-    // 9. Конструкторы
+    
     MagicOrugie();
     MagicOrugie(string n, int d, TypeOrugie t, int du);
 
 
-    // 10. GET-метод для дополнительного урона
     int GetDopUron();
 
 };
