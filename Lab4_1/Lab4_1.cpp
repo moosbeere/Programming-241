3a6e2ae04a1414f7f026844e54c2e79ff6c29406
sralityt-cmyk <sralityt@gmail.com>
2026-01-14 00:42:20 +0300

    1

---
+++ Lab4_1/Lab4_1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>   // для setlocale
+
+using namespace std;
+
+// Класс оружия
+class Weapon {
+private:
+    string name;   // название оружия
+    int damage;    // наносимый урон
+    float weight;  // вес оружия
+
+public:
+    // Конструктор с параметрами
+    Weapon(string n, int d, float w) {
+        name = n;
+        damage = d;
+        weight = w;
+    }
+
+    // Конструктор без параметров
+    // Вызывает конструктор с параметрами и задаёт значения по умолчанию
+    Weapon() : Weapon("Обычный меч", 10, 3.5f) {
+    }
+
+    // Метод вывода информации
+    void printInfo() {
+        cout << "Название оружия: " << name << endl;
+        cout << "Урон: " << damage << endl;
+        cout << "Вес: " << weight << endl;
+        cout << "------------------------" << endl;
+    }
+};
+
+int main() {
+    // Русский язык в консоли
+    setlocale(LC_ALL, "Russian");
+
+    // Объект с параметрами
+    Weapon weapon1("Двуручный топор", 35, 8.2f);
+
+    // Объект без параметров
+    Weapon weapon2;
+
+    // Вывод данных
+    weapon1.printInfo();
+    weapon2.printInfo();
+
+    return 0;
+}
---
+++ Lab4_2/Lab4_2.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <string>
+#include <clocale>
+
+using namespace std;
+
+// Класс оружия
+class Weapon {
+private:
+    string name;
+    int damage;
+    float weight;
+
+public:
+    // Конструктор с параметрами
+    Weapon(string n, int d, float w) {
+        name = n;
+        damage = d;
+        weight = w;
+    }
+
+    // Конструктор без параметров
+    Weapon() : Weapon("Обычный меч", 10, 3.5f) {
+    }
+
+    // Деструктор (пункт 1)
+    ~Weapon() {
+        cout << "Удаляется оружие:" << endl;
+        cout << "Название: " << name << endl;
+        cout << "Урон: " << damage << endl;
+        cout << "Вес: " << weight << endl;
+        cout << "" << endl;
+    }
+
+    // Вывод информации
+    void printInfo() const {
+        cout << "Название: " << name << endl;
+        cout << "Урон: " << damage << endl;
+        cout << "Вес: " << weight << endl;
+    }
+
+    // Пункт 5: проверка, сможет ли поднять оружие
+    bool isTooHeavy(float maxWeight) const {
+        return maxWeight < weight;
+    }
+
+    // Пункт 7: метод, принимающий просто вес
+    float totalWeight(float otherWeight) const {
+        return weight + otherWeight;
+    }
+
+    // Пункт 6 и 8: метод, принимающий объект оружия
+    // Использует перегрузку
+    float totalWeight(const Weapon& other) const {
+        return totalWeight(other.weight);
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    cout << "Обычный объект " << endl;
+    Weapon weapon1("Топор", 30, 7.5f);
+    weapon1.printInfo();
+
+    cout << "Проверка веса (макс. 6): ";
+    cout << (weapon1.isTooHeavy(6) ? "слишком тяжёлый" : "можно поднять") << endl;
+
+    cout << endl << "Динамический объект " << endl;
+    Weapon* weapon2 = new Weapon("Кинжал", 15, 2.0f);
+    weapon2->printInfo();
+
+    cout << "Проверка веса (макс. 3): ";
+    cout << (weapon2->isTooHeavy(3) ? "слишком тяжёлый" : "можно поднять") << endl;
+
+    cout << endl << "Проверка суммарного веса" << endl;
+    cout << "weapon1 + weapon2 = "
+        << weapon1.totalWeight(*weapon2) << endl;
+
+    cout << "weapon1 + вес 5 = "
+        << weapon1.totalWeight(5.0f) << endl;
+
+    // Удаление динамического объекта (пункт 4)
+    delete weapon2;
+
+    cout << endl << "Конец программы" << endl;
+    return 0;
+}
