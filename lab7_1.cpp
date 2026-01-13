21e6f398dcb39f2b151d2dfb91439aa9cc298800
Ulyana <100966619+ulyanka1233@users.noreply.github.com>
2026-01-13 21:28:53 +0300

    Add files via upload

---
+++ lab7_1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void print(string s)
+{
+    cout << s << endl;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    string name;
+    cout << "Введите строку" << endl;
+    cin >> name;
+    //print(name.substr(1, 3));
+    int index = name.find("a");
+    if (index == -1) {
+        cout << "Символа a не найдено";
+    }
+    else {
+        cout << index + 1;
+    }
+}
