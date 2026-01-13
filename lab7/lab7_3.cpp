c9bf8e90279f3d6a489f8bcfbe49803492ba53de
Ulyana <100966619+ulyanka1233@users.noreply.github.com>
2026-01-13 21:32:39 +0300

    Add files via upload

---
+++ lab7/lab7_3.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#define _USE_MATH_DEFINES
+#include <math.h>
+#include <iomanip> 
+#include <fstream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	float r;
+	cout << "";
+	cin >> r;
+	cout << setprecision(3) << 2 * r * M_PI * r << endl;
+
+	fstream fs;
+	while (true) {
+		cout << "";
+		string line;
+		cin >> line;
+		if (line == "read") {
+			fs.open("a.txt", ios::in);
+			string str;
+			string data;
+			while (getline(fs, str)) {
+				cout << str << endl;
+				data += str;
+			}
+			fs.close();
+		}
+		else if (line == "erase") {
+			fs.open("a.txt", ios::out);
+			fs.close();
+		}
+		else if (line == "exit") {
+			break;
+		}
+		else
+		{
+			fs.open("a.txt", ios::app);
+			fs << line << endl;
+			fs.close();
+		}
+
+	}
+}
