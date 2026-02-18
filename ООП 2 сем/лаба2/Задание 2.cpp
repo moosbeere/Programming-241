be3db21fc6d30f7ea46fbb95e3004e9893a55a7c
niluning <nilufartt50@gmail.com>
2026-02-18 23:16:24 +0300

    Add files via upload

---
+++ ООП 2 сем/лаба2/Задание 2.cpp
@@ -0,0 +1,30 @@
+#include "Vehicle.h"
+#include <iostream>
+
+Vehicle::Vehicle(const std::string& brand, int maxSpeed)
+    : brand(brand), maxSpeed(maxSpeed), currentSpeed(0) {}
+
+void Vehicle::accelerate(int increment) {
+    if (increment > 0) {               // защита от отрицательного ускорения
+        currentSpeed += increment;
+        if (currentSpeed > maxSpeed) {
+            currentSpeed = maxSpeed;    // ограничиваем максимальной скоростью
+        }
+    }
+}
+
+void Vehicle::brake(int decrement) {
+    if (decrement > 0) {               // защита от отрицательного торможения
+        currentSpeed -= decrement;
+        if (currentSpeed < 0) {
+            currentSpeed = 0;           // не даём уйти в минус
+        }
+    }
+}
+
+void Vehicle::getStatus() const {
+    std::cout << "Марка: " << brand << "\n"
+              << "Максимальная скорость: " << maxSpeed << " км/ч\n"
+              << "Текущая скорость: " << currentSpeed << " км/ч\n"
+              << "------------------------------\n";
+}
