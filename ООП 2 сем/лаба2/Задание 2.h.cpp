6ee08fc7e6c28f538aedd259eaae67bc62925148
niluning <nilufartt50@gmail.com>
2026-02-18 23:12:53 +0300

    Add files via upload

---
+++ ООП 2 сем/лаба2/Задание 2.h.cpp
@@ -0,0 +1,41 @@
+#ifndef VEHICLE_H
+#define VEHICLE_H
+
+#include <string>
+
+/**
+ * Класс "Транспортное средство"
+ */
+class Vehicle {
+private:
+    std::string brand;      // марка (приватное поле)
+    int maxSpeed;           // максимальная скорость (приватное поле)
+    int currentSpeed;       // текущая скорость (приватное поле)
+
+public:
+    /**
+     * Конструктор с параметрами.
+     * @param brand марка
+     * @param maxSpeed максимальная скорость
+     */
+    Vehicle(const std::string& brand, int maxSpeed);
+
+    /**
+     * Увеличить текущую скорость на increment (но не выше maxSpeed).
+     * @param increment величина увеличения
+     */
+    void accelerate(int increment);
+
+    /**
+     * Уменьшить текущую скорость на decrement (но не ниже 0).
+     * @param decrement величина уменьшения
+     */
+    void brake(int decrement);
+
+    /**
+     * Вывести информацию о транспортном средстве.
+     */
+    void getStatus() const;
+};
+
+#endif // VEHICLE_H
