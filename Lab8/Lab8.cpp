a571f9f31eec08db2fab46c803ba6676dcc0d477
MrZnkrscht <74868601+babidjon872@users.noreply.github.com>
2026-02-19 13:08:37 +0300

    Add files via upload

---
+++ Lab8/Lab8.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+#include "Warehouse.h"
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    Warehouse<int> intWarehouse;
+    intWarehouse.addItem(101);
+    intWarehouse.addItem(202);
+    intWarehouse.addItem(303);
+
+    cout << "Склад целых чисел, всего грузов: " << intWarehouse.getTotalItems() << endl;
+    for (int i = 0; i < intWarehouse.getTotalItems(); ++i) {
+        cout << "  Элемент " << i << ": " << intWarehouse.getItem(i) << endl;
+    }
+
+    Warehouse<string> stringWarehouse;
+    stringWarehouse.addItem("первый");
+    stringWarehouse.addItem("второй");
+    stringWarehouse.addItem("третий");
+
+    cout << "\nСклад строк, всего грузов: " << stringWarehouse.getTotalItems() << endl;
+    for (int i = 0; i < stringWarehouse.getTotalItems(); ++i) {
+        cout << "  Элемент " << i << ": " << stringWarehouse.getItem(i) << endl;
+    }
+
+    try {
+        cout << "\nПробуем получить элемент с индексом 5: ";
+        cout << intWarehouse.getItem(5) << endl;
+    }
+    catch (out_of_range& err) {
+        cout << "Ошибка: " << err.what() << endl;
+    }
+
+    return 0;
+}
