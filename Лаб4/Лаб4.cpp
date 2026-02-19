912918c150bdb70d3bb1f7ece29b37e023132cb0
turkovr0623 <turkovroman2006@mail.ru>
2026-02-19 13:33:52 +0300

    Add files via upload

---
+++ Лаб4/Лаб4.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class TransportUnit {
+public:
+
+    virtual double calculateToll(double distance) const = 0;
+};
+
+
+class Car : public TransportUnit {
+public:
+    double calculateToll(double distance) const override {
+        return distance * 0.1;
+    }
+};
+
+
+class Truck : public TransportUnit {
+public:
+    double calculateToll(double distance) const override {
+        return distance * 0.3 + 5.0;
+    }
+};
+
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    Car car;
+    Truck truck;
+
+    TransportUnit* units[2];
+
+    units[0] = &car;
+    units[1] = &truck;
+
+    double distance = 100;
+
+    cout << "Расчет платы за " << distance << " км:\n";
+
+    for (int i = 0; i < 2; i++) {
+        cout << "Плата: " << units[i]->calculateToll(distance) << endl;
+    }
+
+    return 0;
+}
