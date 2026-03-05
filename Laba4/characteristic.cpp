886257d7c03ff54a8b5fd6495d897da978feaeb8
K1ryusha2123 <kirill.bondarenko.2110@gmail.com>
2026-03-05 12:36:35 +0300

    Delete Laba4 directory

--- Laba4/characteristic.cpp
+++
@@ -1,7 +0,0 @@
-#include "characteristic.h"
-
-Characteristic::Characteristic(int strength) : strength(strength) {}
-
-int Characteristic::GetDamage(const Weapon& weapon) const {
-    return strength + weapon.getDamage();
-}
