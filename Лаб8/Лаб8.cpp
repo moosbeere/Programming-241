4c439ece5b116e524c4f9dc1b302fb54101e025b
turkovr0623 <turkovroman2006@mail.ru>
2026-03-11 23:44:04 +0300

    Add files via upload

---
+++ Лаб8/Лаб8.cpp
@@ -0,0 +1,54 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+template <typename T>
+class Warehouse {
+private:
+    vector<T> items;
+
+public:
+    void addItem(const T& item) {
+        items.push_back(item);
+    }
+
+    T getItem(int index) {
+        return items.at(index);
+    }
+
+    int getTotalItems() {
+        return items.size();
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "RU");
+
+    Warehouse<int> number;
+
+    number.addItem(101);
+    number.addItem(202);
+    number.addItem(303);
+
+    cout << "Склад номеров грузов:" << endl;
+    for (int i = 0; i < number.getTotalItems(); i++) {
+        cout << number.getItem(i) << endl;
+    }
+
+    cout << endl;
+
+    Warehouse<string> name;
+
+    name.addItem("Телевизоры");
+    name.addItem("Компьютеры");
+    name.addItem("Телефоны");
+
+    cout << "Склад названий грузов:" << endl;
+    for (int i = 0; i < name.getTotalItems(); i++) {
+        cout << name.getItem(i) << endl;
+    }
+
+    return 0;
+}
