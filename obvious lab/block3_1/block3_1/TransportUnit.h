014f8ae7483bdef81f85f601713a213e2865656a
erajjan <rauf2017.18@gmail.com>
2026-03-15 10:35:19 +0300

    d1

--- obvious lab/block3_1/block3_1/TransportUnit.h
+++ obvious lab/block3_1/block3_1/TransportUnit.h
@@ -1,35 +1,16 @@
-#pragma once
+#pragma once 
+#include <iostream>
 #include <string>
 using namespace std;
 
+class DispatchCenter {
+public:
+	static int totalVehiclesDispatched;
+	static void report();
+};
+
 class TransportUnit {
-private:
-    string licensePlate;
-
 public:
-    static int totalVehiclesDispatched;
-
-    TransportUnit(string plate);
-    virtual double calculateToll(double distance) const = 0;
-    virtual string getType() const = 0;
-    string getLicensePlate() const { return licensePlate; }
-    static void report();
-    virtual ~TransportUnit();
-};
-
-class Car : public TransportUnit {
-public:
-    Car(string plate);
-    double calculateToll(double distance) const override;
-    string getType() const override;
-};
-
-class Truck : public TransportUnit {
-private:
-    double capacity;
-
-public:
-    Truck(double cap, string plate);
-    double calculateToll(double distance) const override;
-    string getType() const override;
+	string b;
+	TransportUnit( string b);
 };
--- obvious lab/block3_1/block3_1/block3_1.cpp
+++ obvious lab/block3_1/block3_1/block3_1.cpp
@@ -1,36 +1,15 @@
-﻿#include <iostream>
-#include <vector>
-#include "TransportUnit.h"
+﻿#include <iostream>;
+#include"TransportUnit.h"
+
 using namespace std;
 
-int main() {
-    setlocale(LC_ALL, "RU");
+int main(){
+	setlocale(LC_ALL, "RU");
 
-    vector<TransportUnit*> garage;
+	DispatchCenter::report();
 
-    cout << "СОЗДАНИЕ ТРАНСПОРТА" << endl;
+	TransportUnit V1("kamaz");
+	TransportUnit V2("HAVAL");
 
-    garage.push_back(new Car("123qwer"));
-    garage.push_back(new Car("0987po"));
-    garage.push_back(new Truck(20.5, "lkjhg78"));
-    garage.push_back(new Truck(8.0, "123fvdv"));
-
-    cout << "ИНФОРМАЦИЯ О ТРАНСПОРТЕ" << endl;
-
-    for (size_t i = 0; i < garage.size(); i++) {
-        cout << i + 1 << ". " << garage[i]->getType() << endl;
-        cout << "   Плата за 100км: "
-            << garage[i]->calculateToll(100) << " у.е." << endl;
-        cout << "   Номер: " << garage[i]->getLicensePlate() << endl;
-    }
-
-    TransportUnit::report();
-
-    for (auto v : garage) {
-        delete v;
-    }
-
-    TransportUnit::report();
-
-    return 0;
+	DispatchCenter::report();
 }
