e4953df96687e190187eacb0aa97f1df3f2d284b
Ulyana <100966619+ulyanka1233@users.noreply.github.com>
2026-03-19 13:30:17 +0300

    Add files via upload

---
+++ лаба5/main.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include "Car.h"
+#include "AssignedRoute.h"
+
+int main() {
+    Car car;
+    Route route("Москва", "Санкт-Петербург", 700.0);
+
+    AssignedRoute assigned(route, &car);
+
+    assigned.printAssignedInfo();
+
+    return 0;
+}
