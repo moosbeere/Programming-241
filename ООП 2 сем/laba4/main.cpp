0307e3d6c50dc8a03a1480de8c81ce6a858a51ec
niluning <nilufartt50@gmail.com>
2026-03-26 13:17:03 +0300

    Update main.cpp

--- ООП 2 сем/laba4/main.cpp
+++ ООП 2 сем/laba4/main.cpp
@@ -1,33 +1,29 @@
-#include <iostream>      
-#include "transport.h"   
+#include <iostream>
+#include "Car.h"
+#include "Truck.h"
 
 int main() {
     
     TransportUnit* units[4];
 
-    // Заполняем массив объектами производных классов
-   
-    units[0] = new Car();    
-    units[1] = new Truck();  
-    units[2] = new Car();    
-    units[3] = new Truck();  
+    // Создаём объекты производных классов
+    units[0] = new Car();
+    units[1] = new Truck();
+    units[2] = new Car();
+    units[3] = new Truck();
 
-    double distance = 100.0; 
+    double distance = 100.0;  
 
-    // Цикл по всем элементам массива
+    // Вычисляем и выводим плату для каждого объекта
     for (int i = 0; i < 4; ++i) {
-       
         double toll = units[i]->calculateToll(distance);
-
-       
         std::cout << "Плата для объекта " << i << ": " << toll << " у.е." << std::endl;
     }
 
-    // Освобождаем память, выделенную под объекты
+   
     for (int i = 0; i < 4; ++i) {
-        delete units[i];  // Вызов delete через указатель на базовый класс
-       
+        delete units[i];
     }
 
-    return 0;  
+    return 0;
 }
