4c318f45258be7ccef2edb487c91bcd943e5cab7
Ulyana <100966619+ulyanka1233@users.noreply.github.com>
2026-03-26 13:38:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include "Vehicle.h"
+
+// обьявляем стат поле
+int DispatchCenter::totalVehiclesDispatched = 0;
+
+int main() {
+    
+    Vehicle v1;
+    Vehicle v2;
+    Vehicle v3;
+
+   
+    DispatchCenter::report();
+
+  
+    Vehicle v4;
+
+   
+    DispatchCenter::report();
+
+    return 0;
+}
