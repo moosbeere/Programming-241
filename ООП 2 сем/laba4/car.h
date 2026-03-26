11560f9705bede17d098a0c6a26fdc8d83ab300b
niluning <nilufartt50@gmail.com>
2026-03-26 13:12:20 +0300

    Update car.h

--- ООП 2 сем/laba4/car.h
+++ ООП 2 сем/laba4/car.h
@@ -3,11 +3,10 @@
 
 #include "TransportUnit.h"
 
-// Класс легкового автомобиля
 class Car : public TransportUnit {
 public:
     // Переопределение чисто виртуальной функции
     double calculateToll(double distance) const override;
 };
 
-#endif // CAR_H
+#endif 
