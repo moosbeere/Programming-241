0d8a8567bb52c5c6cec6873576f015482a31085f
niluning <nilufartt50@gmail.com>
2026-03-26 13:08:58 +0300

    Update transport.h

--- ООП 2 сем/laba4/transport.h
+++ ООП 2 сем/laba4/transport.h
@@ -1,26 +1,12 @@
-#ifndef TRANSPORT_H  
-#define TRANSPORT_H
+#ifndef TRANSPORT_UNIT_H
+#define TRANSPORT_UNIT_H
 
 class TransportUnit {
 public:
+  
     virtual double calculateToll(double distance) const = 0;
 
-   
     virtual ~TransportUnit() {}
 };
 
-class Car : public TransportUnit {
-public:
-    // Переопределяем чисто виртуальную функцию
-    // Плата = distance * 0.1
-    double calculateToll(double distance) const override;
-};
-
-class Truck : public TransportUnit {
-public:
-    // Переопределяем чисто виртуальную функцию
-    // Плата = distance * 0.3 + 5.0
-    double calculateToll(double distance) const override;
-};
-
-#endif // TRANSPORT_H
+#endif // TRANSPORT_UNIT_H
