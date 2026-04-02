c4b9b2f4e4a5278bb67b0892a34b3faac48e3b50
turkovr0623 <turkovroman2006@mail.ru>
2026-04-02 13:08:10 +0300

    Delete Лаб11.2 directory

--- Лаб11.2/FreightVehicle.cpp
+++
@@ -1,7 +0,0 @@
-﻿#include "FreightVehicle.h"
-
-FreightVehicle::FreightVehicle(double c) : cargoCapacity(c) {}
-
-double FreightVehicle::getCargoCapacity() const {
-    return cargoCapacity;
-}
--- Лаб11.2/Vehicle.h
+++
@@ -1,8 +0,0 @@
-﻿#pragma once
-
-class Vehicle {
-public:
-    virtual double getCargoCapacity() const {
-        return 0;
-    }
-};
