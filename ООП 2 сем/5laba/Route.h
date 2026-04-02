af8e6336dc7d783113546d1d5f4d5f17d1036050
niluning <nilufartt50@gmail.com>
2026-04-02 12:44:35 +0300

    Update Route.h

--- ООП 2 сем/5laba/Route.h
+++ ООП 2 сем/5laba/Route.h
@@ -1,1 +1,24 @@
+#ifndef ROUTE_H
+#define ROUTE_H
 
+#include <string>
+
+class Route {
+private:
+    std::string startPoint;
+    std::string endPoint;
+    double length;   // в км
+
+public:
+    
+    Route(const std::string& start, const std::string& end, double len);
+
+    // Метод вывода информации
+    void printInfo() const;
+
+    double getLength() const;
+    const std::string& getStartPoint() const;
+    const std::string& getEndPoint() const;
+};
+
+#endif
