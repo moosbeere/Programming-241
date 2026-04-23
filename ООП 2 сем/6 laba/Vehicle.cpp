ce71761eebcecfe659d2cb7bf423ba38ee14e4cc
niluning <nilufartt50@gmail.com>
2026-04-23 12:55:29 +0300

    Update Vehicle.cpp

--- ООП 2 сем/6 laba/Vehicle.cpp
+++ ООП 2 сем/6 laba/Vehicle.cpp
@@ -4,10 +4,10 @@
 
 Vehicle::Vehicle(const std::string& m, const std::string& n)
     : model(m), number(n) {
-    registerVehicle();   // регистрируем при создании объекта
+    registerVehicle();   // регистр при создании объекта
 }
 
 void Vehicle::registerVehicle() {
-    DispatchCenter::totalVehiclesDispatched++;   // увеличиваем статическое поле
+    DispatchCenter::totalVehiclesDispatched++;   //увеличиваем статическое поле
     std::cout << "Зарегистрировано: " << model << " (" << number << ")" << std::endl;
 }
