5e356d6450f239c0650b6b3e2aefc978655fcfb5
niluning <nilufartt50@gmail.com>
2026-05-07 13:38:46 +0300

    Update Warehouse.h

--- ООП 2 сем/лаба 8/Warehouse.h
+++ ООП 2 сем/лаба 8/Warehouse.h
@@ -1,1 +1,28 @@
+#ifndef WAREHOUSE_H
+#define WAREHOUSE_H
 
+#include <vector>
+
+template<typename T>
+class Warehouse {
+private:
+    std::vector<T> items;
+    
+public:
+    void addItem(const T& item) {
+        items.push_back(item);
+    }
+    
+    T getItem(int index) {
+        if (index < 0 || index >= (int)items.size()) {
+            throw "Нет такого груза!";
+        }
+        return items[index];
+    }
+    
+    int getTotalItems() {
+        return items.size();
+    }
+};
+
+#endif
